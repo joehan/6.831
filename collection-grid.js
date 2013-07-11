dc71d1f4732cd62c02a62224b108241286cc6bcf
Megan O'Leary <olearym@mit.edu>
2013-07-11 16:54:25 -0400

    can create multiple collections linked to main group (slight aesthetic issues)

--- collection-grid.js
+++ collection-grid.js
@@ -69,20 +69,81 @@ function getEverything(){
 return {'getValues':getValues, 'parseData':parseData, 'findURLColumn':findURLColumn, 'getURLList':getURLList}
 }
 
+var showNamePrompt = function() {
+  
+  var maxCollections = 4;
+  var numCollections = $('.collections').children().length;
+
+  if (numCollections <= maxCollections) {
+    $('.collection-name').css("visibility", "visible");
+  } 
+
+}
+
+var createCollection = function() {
+
+  collectionNumber = $('.collections').children().length;
+
+  collectionName = $('.collection-name-input')[0].value;
+  $('.collection-name-input').val("")
+  $('.collection-name').css("visibility", "hidden");
+
+  newTab = $('<li><a href=#'+collectionName+' data-toggle="tab">'+collectionName+'</a></li)');
+  newTabBody = $('<div id='+collectionName+' class="tab-pane">this is the '+collectionName+' tab</div>')
+  newSortable = $('<ul id="sortable'+collectionNumber+'" class="connected'+collectionNumber+' group"></ul>')
+  newTabBody.append(newSortable);
+
+  $('.collections').append(newTab);
+  $('.collections-content').append(newTabBody)
+
+  $('#sortable-main').sortable({
+    connectWith: ".connected"+collectionNumber,
+   }).disableSelection();
+  $('#sortable'+collectionNumber).sortable({
+    connectWith: ".connected-main"
+   }).disableSelection();
+
+}
+
+var deleteCollection = function() {
+
+}
+
 
 var iframeMaker = (function() {
 
-	exports = {}
+	var exports = {}
+
+  var showModal = function() {
+
+    $('.modal-body').empty();
+    $('.modal-footer').empty();
+
+    var URL = "'"+$($($($($(this)).parent()).parent()).find('iframe')).attr("src")+"'";
+    console.log(URL)
+    var iframeDiv = $('<div class="modal-iframe-holder"></div>')
+    var modalIframe = $('<iframe class="modal-iframe" sandbox="" width="1000" height="750" src='+URL+' style="-webkit-transform:scale(0.5);-moz-transform-scale(0.5);">')
+    var URLbutton = $('<button class="btn btn-primary" onclick="window.open('+URL+');">Visit Site</button>')
+    var closeButton = $('<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>')
+    iframeDiv.append(modalIframe)
+    $('.modal-body').append(iframeDiv)
+    $('.modal-footer').append(closeButton, URLbutton)
+  }
 
 	var setup = function(div) {
-		for (i=1;i<URLList.length;i++) {
-			var li = $('<li class = "iframe ui-state-default">')
+		
+    for (i=1;i<URLList.length;i++) {
+			
+      var link = $('<a data-toggle="modal" data-target="#myModal"></a>')
+      var li = $('<li class = "iframe ui-state-default">')
 			var overlay = $('<div class="overlay"></div>')
 			var iframe = $('<iframe class="body-iframe" sandbox="" width="1000" height="750" src='+URLList[i]+' style="-webkit-transform:scale(0.24);-moz-transform-scale(0.25);">')
-			// var buttons = $('<div class="buttons"><button class="one">1</button><button class="two">2</button><button class="three">3</button></div>')
-			// overlay.append(buttons)
-			li.append(overlay, iframe)
+
+			link.append(overlay)
+      overlay.on("click", showModal)
+      li.append(link, iframe)
 			div.append(li)
+
 		}
 	}
 	exports.setup = setup
@@ -96,9 +157,16 @@ $.getJSON( 'https://spreadsheets.google.com/feeds/cells/0AgtGE4FgUNk1dERRcF9RTU9
         JSONdata=data
     }).done(function(){getEverything().getURLList();
                        console.log(items);
-                       $("#sortable1").each(function() {
+                       $("#sortable-main").each(function() {
 		                      iframeMaker.setup($(this));
 	                   })
+                       $('.new-collection').on("click", showNamePrompt)
+                        $('.name-submit').on("click", createCollection)
+                        $('.collection-name-input').keydown(function(event){
+                          if(event.keyCode == 13) {
+                            $('.name-submit').click();
+                          }
+                        })
                       })
 
 //$(document).ready(function() {
