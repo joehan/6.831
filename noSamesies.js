3a0e49d3902b80e43f70af9a8b0f3190391f1ed2
Joe Hanley <jhanley@mit.edu>
2013-07-11 13:00:59 -0400

    pushing and pulling

--- noSamesies.js
+++ noSamesies.js
@@ -1,9 +1,36 @@
-var removeDupes = function(URLList){
-    testList = []
+var removeDupes = function(){
+    var testList = []
     for (i=0;i<URLList.length;i++){
-        if (testList.indexOf(URLList[i])=-1)
+        if (testList.indexOf(URLList[i])==-1)
             testList.push(URLList[i])
+            
     }
     console.log(testList)
     URLList=testList
 }
+
+var addProtocol = function(URL){
+    if (URL.slice(0,4)!= 'http'){
+        URL = 'http://'+URL
+    }
+    return URL
+}
+var getSpreadsheetInfo = function(URL){
+    var infoList = []
+    for (i=1; i<=items.length;i++){
+        if (items[i]['URL'] == URL){
+            commentsList.push(items[i])
+        }
+            
+    }
+    return infoList
+}
+
+var getComments = function(URL){
+    commentsList = []
+    var infoList = getSpreadsheetInfo(URL)
+    for (i=0;i<infoList.length;i++){
+        commentsList.push(infoList[i]['Comments'])
+    }
+}
+
